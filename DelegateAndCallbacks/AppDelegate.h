22ad4b5dc478ceb28255ba99cd72628f4dc55618
Matt Gillingham <mgillingham@mediweb.jp>
2015-03-13 09:45:27 +0900

    Initial Commit

---
+++ DelegateAndCallbacks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateAndCallbacks
+//
+//  Created by Matt on 3/13/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegateAndCallbacks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateAndCallbacks
+//
+//  Created by Matt on 3/13/15.
+//  Copyright (c) 2015 Matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
